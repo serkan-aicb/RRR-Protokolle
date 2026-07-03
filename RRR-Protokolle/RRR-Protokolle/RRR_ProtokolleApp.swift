fb1a3d79c43149c919381714849f77812d076cde
serkan-aicb <serkan@aicoreblock.com>
2026-07-03 21:14:04 +0300

    Xcode Install

---
+++ RRR-Protokolle/RRR-Protokolle/RRR_ProtokolleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RRR_ProtokolleApp.swift
+//  RRR-Protokolle
+//
+//  Created by CORVUS on 03.07.26.
+//
+
+import SwiftUI
+
+@main
+struct RRR_ProtokolleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RRR-Protokolle/RRR-ProtokolleTests/RRR_ProtokolleTests.swift
@@ -0,0 +1,19 @@
+//
+//  RRR_ProtokolleTests.swift
+//  RRR-ProtokolleTests
+//
+//  Created by CORVUS on 03.07.26.
+//
+
+import Testing
+@testable import RRR_Protokolle
+
+struct RRR_ProtokolleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
